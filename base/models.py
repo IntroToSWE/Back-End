e24b020219c1eb1c8a76ca3e6856878b997ecf9a
Ryan Acuna <racuna@unomaha.edu>
2024-04-01 23:25:00 -0500

    added new user model and edited plant relationships in tables

--- base/models.py
+++ base/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
 
 # database table for personal plants
 class personalPlant(models.Model):
     # attributes
+    # id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) 
-    name = models.CharField(max_length=200)
-    size = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
-    inside = models.BooleanField(default=True)
-    water = models.CharField(max_length=200)
-    sun = models.CharField(max_length=200)
-    fertilization= models.CharField(max_length=200)
-    soil = models.CharField(max_length=200)
+    plantID = models.ForeignKey("libraryPlant", on_delete=models.CASCADE) # we can use this ID to reference the plant info in sep. table
     alive = models.BooleanField(default=False)
+    # name = models.CharField(max_length=200)
+    # size = models.CharField(max_length=50)
+    # description = models.TextField(null=True, blank=True)
+    # inside = models.BooleanField(default=True)
+    # water = models.CharField(max_length=200)
+    # sun = models.CharField(max_length=200)
+    # fertilization= models.CharField(max_length=200)
+    # soil = models.CharField(max_length=200)
+    # alive = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -23,12 +26,11 @@ class personalPlant(models.Model):
     class Meta:
         app_label = 'base'
 
-        
-
 
 # databse for plant library 
 class libraryPlant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) 
+    # id = models.AutoField(primary_key=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) 
     name = models.CharField(max_length=200)
     scientific_name = models.CharField(max_length=200)
     size = models.CharField(max_length=50)
@@ -46,5 +48,11 @@ class libraryPlant(models.Model):
     def __str__(self):
         return self.name
 
+class Users(models.Model):
+    # id = models.AutoField(primary_key=True, editable=False)
+    first_name = models.CharField(max_length=75, blank=True, default='')
+    last_name = models.CharField(max_length=75, blank=True, default='')
+    email = models.EmailField(max_length=150, blank=True, default='', unique=True)
+    password = models.CharField(max_length=50, default='', null=False)
 
 
