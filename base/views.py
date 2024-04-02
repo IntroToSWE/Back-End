35a5ff362cc656cdc3dea607039b5c58e9645569
Ryan Acuna <racuna@unomaha.edu>
2024-04-01 23:25:36 -0500

    added views for urls and get method for plant library to return all db plants

--- base/views.py
+++ base/views.py
@@ -1,3 +1,50 @@
+import json
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core import serializers
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+class Loginview(APIView):
+    def get(self, request):
+        return Response("no GET method logic, but working fine", status=200)
+        # return Response(request.GET.get("email"), status=200)
+
+    def post(self, request):
+        jsonData = request.data
+        email = jsonData["email"]
+        password = jsonData["password"]
+        output = Users.objects.filter(email=email, password=password)
+        if not output:
+            print("empty, no account found")
+            return Response("No User Found", status=403)
+        else:
+            print("not empty")
+            return Response("User Found", status=202)
+        
+class SignupView(APIView):
+    def get(self, request):
+        return Response("no GET method logic, but working fine", status=200)
+
+    def post(self, request):
+        print(request.data)
+        return Response("User Created", status=200)
+
+class HomeView(APIView):
+    def get(self, request):
+        return Response()
+    
+    def post(self, request):
+        return Response()
+    
+class PlantLibraryView(APIView):
+    def get(self, request):
+        output = libraryPlant.objects.all()
+        jsonOutput = serializers.serialize('json', output)
+        jsonOutput = json.loads(jsonOutput)
+        return Response(jsonOutput)
+        # return HTTPResponse(jsonOutput, content_type="text/json-comment-filtered") # might use this instead ???
+    
+    def post(self, request):
+        return Response()
