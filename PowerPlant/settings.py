41ae453a330368eba2658525d8617dacdfd88378
Ryan Acuna <racuna@unomaha.edu>
2024-04-01 21:47:17 -0500

    added trusted origins

--- PowerPlant/settings.py
+++ PowerPlant/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'base',
+    'PowerPlant',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,6 +72,19 @@ TEMPLATES = [
         },
     },
 ]
+
+# change this to live server
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080", #change to your local host for frontend
+    "http://ec2-3-14-13-63.us-east-2.compute.amazonaws.com"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8080",  #change to your local host for frontend
+    "http://ec2-3-14-13-63.us-east-2.compute.amazonaws.com"
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'PowerPlant.wsgi.application'
 
