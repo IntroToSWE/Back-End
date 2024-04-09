f3ac035ecfbac21573a3671c62b83b06eab26de8
Mjames951 <aspc.james2@gmail.com>
2024-04-09 00:03:25 -0500

    removed douple checking on UpdateUserPlant

--- base/views.py
+++ base/views.py
@@ -154,15 +154,12 @@ class UpdateUserPlants(APIView):
         plantID = jsonData["plantID"]
         alive = jsonData["alive"] # should be an interger (1 = alive, 0 = not alive)
         if "delete" in jsonData:
-            if jsonData["delete"].lower() == "true":
-                query = f"""DELETE FROM base_personalplant WHERE rowid = (SELECT rowid FROM base_personalplant WHERE user_id={userID} AND plantID_id={plantID} AND alive={alive} LIMIT 1);"""
+            query = f"""DELETE FROM base_personalplant WHERE rowid = (SELECT rowid FROM base_personalplant WHERE user_id={userID} AND plantID_id={plantID} AND alive={alive} LIMIT 1);"""
 
-                with connection.cursor() as cursor:
-                    cursor.execute(query)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
 
                 return Response("User Deleted", status=200)
-            else:
-                return Response("Nothing Happened", status=200)
         elif "update" in jsonData:
             plant = personalPlant.objects.filter(user_id=userID, plantID_id=plantID, alive=alive)
 
